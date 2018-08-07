21e13de59e84ee0566ca55b45e546c746c50bd2a
Rooming <34074975+IRoomingI@users.noreply.github.com>
2018-08-07 17:00:23 +0200

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,3 +1,3 @@
-TOKEN = "Mzk2NzIwNjQzNDE4MzU3NzYy.Dks9ZA.zMrAUjYxTi8p_mZyPZonAgnJueY"   # Discord Bot Token
+TOKEN = ""   # Discord Bot Token
 PREFIX = "+"    # Command Prefix; example: +ping
 GAME = ""     # enter what should be shown after "Playing ..." below the Discord Username
